6101d13d54084cc16f19ae2c9d28d284afcfd008
Shaman <infografika@inbox.ru>
2023-01-05 19:33:09 +0700

    add all classes

--- lesson2/Student.java
+++ lesson2/Student.java
@@ -1,34 +1,22 @@
 package lesson2;
 
 public class Student {
-  private String getFirstName;
-  private String getSecondName;
-  private float getAverageGrade;
+  private String FirstName;
+  private String SecondName;
+  private float AverageGrade;
 
-  public Student(String getFirstName, String getSecondName, float getAverageGrade) {
-    this.getFirstName = getFirstName;
-    this.getSecondName = getSecondName;
-    this.getAverageGrade = getAverageGrade;
-  }
-
-  public String getGetFirstName() {
-    return getFirstName;
-  }
-
-  public String getGetSecondName() {
-    return getSecondName;
-  }
-
-  public float getGetAverageGrade() {
-    return getAverageGrade;
+  public Student(String FirstName, String SecondName, float AverageGrade) {
+    this.FirstName = FirstName;
+    this.SecondName = SecondName;
+    this.AverageGrade = AverageGrade;
   }
 
   @Override
   public String toString() {
     return "Student{" +
-        "getFirstName='" + getFirstName + '\'' +
-        ", getSecondName='" + getSecondName + '\'' +
-        ", getAverageGrade=" + getAverageGrade +
+        "getFirstName='" + FirstName + '\'' +
+        ", getSecondName='" + SecondName + '\'' +
+        ", getAverageGrade=" + AverageGrade +
         '}';
   }
 }
