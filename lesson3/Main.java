bed52cd4cb60cd4eedff0da34b78653faaaf1086
Shaman <infografika@inbox.ru>
2023-01-07 20:01:16 +0700

    add Student

--- lesson3/Main.java
+++ lesson3/Main.java
@@ -13,5 +13,38 @@ package lesson3;
 //    • Он движется справа налево
 //    Проверьте, как он работает.
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+
 public class Main {
+  public static void main(String[] args) {
+    Student st1 = new Student("Denis", LocalDate.now(), 4.5f);
+    Student st2 = new Student("Masha", LocalDate.now(), 4.1f);
+    Student st3 = new Student("Anna", LocalDate.now(), 3.6f);
+    Student st4 = new Student("Petr", LocalDate.now(), 4.9f);
+
+    List<Student> list = new ArrayList<>();
+    list.add(st1);
+    list.add(st2);
+    list.add(st3);
+    list.add(st4);
+
+
+    StudentGroup studentGroup = new StudentGroup(list);
+
+//        for (Student student : studentGroup ) {
+//            System.out.println(student);
+//        }
+
+    Iterator<Student> iter = list.iterator();
+
+    while (iter.hasNext()) {
+      System.out.println(iter.next());
+    }
+  }
+
 }
---
+++ lesson3/StudentGroup.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroup implements Iterable<Student> {
+    private List<Student> studentList;
+
+    public StudentGroup(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public int studentCount() {
+        return studentList.size();
+    }
+
+    public Student nextStudent(int index) {
+        return studentList.get(index);
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        return studentList.iterator();
+    }
+}
